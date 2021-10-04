528cbd5defc3aacdc8a1be5a246b4e41f7f7fb54
gabrielgordbegli <ggordbegli@gmail.com>
2021-10-04 08:11:07 -0400

    added local converter

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function convert(){
+    let hours = parseInt(document.getElementById('hours_input').value);
+    let minutes = document.getElementById('minutes_input').value;
+    hours += 4;
+    console.log(hours);  
+    if(hours <= 12){
+        let output = ("= " + hours.toString() + ":" + minutes + " pm");
+        document.getElementById('output').innerText = output;
+    }
+    if(hours > 12){
+        hours -= 12;
+        let output = ("= " + hours.toString() + ":" + minutes + " am");
+        document.getElementById('output').innerText = output;
+    }
+}
