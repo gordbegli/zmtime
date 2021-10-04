8bb757de3a52c7f74a9b94e1944e332ba236ab71
gabrielgordbegli <ggordbegli@gmail.com>
2021-10-04 09:53:54 -0400

    moved convert to sciprts.js and fixed functionality for past 12am

--- myscripts.js
+++ myscripts.js
@@ -3,12 +3,16 @@ function convert(){
     let minutes = document.getElementById('minutes_input').value;
     hours += 4;
     console.log(hours);  
-    if(hours <= 12){
+    if(hours < 12){
         let output = ("= " + hours.toString() + ":" + minutes + " pm");
         document.getElementById('output').innerText = output;
     }
-    if(hours > 12){
+    if(hours = 12){
         let output = ("= " + hours.toString() + ":" + minutes + " am");
         document.getElementById('output').innerText = output;
     }
-}
+    if(hours > 12){
+        hours -= 12;
+        let output = ("= " + hours.toString() + ":" + minutes + " am");
+        document.getElementById('output').innerText = output;
+    }}
